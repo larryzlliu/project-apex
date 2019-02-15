9e51343fcfb31aaec420c4281e378359ffb79a04
Larry Liu <liu.larry96@gmail.com>
2019-02-15 11:47:36 -0500

    Added toggle record button with no functionality

--- app/src/main/java/connected_coop/projectapex/MainActivity.kt
+++ app/src/main/java/connected_coop/projectapex/MainActivity.kt
@@ -2,11 +2,24 @@ package connected_coop.projectapex
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRecord = findViewById<ToggleButton>(R.id.button_record_toggle)
+
+        btnRecord.setOnCheckedChangeListener{_, isRecording ->
+            if (isRecording) {
+                Toast.makeText(this, "Recording...", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Stopping...", Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
 }
