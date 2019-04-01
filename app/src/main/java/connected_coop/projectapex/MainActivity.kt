e6b808df8d9dd7672f3bb8638cfb276bfd80a3fb
Megan Bosiljevac <meganbosiljevac@gmail.com>
2019-04-01 19:32:19 -0400

    changed toggle button to image button

--- app/src/main/java/connected_coop/projectapex/MainActivity.kt
+++ app/src/main/java/connected_coop/projectapex/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.provider.Settings
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,13 +19,18 @@ class MainActivity : AppCompatActivity() {
     private val speechUtil = SpeechUtil()
 
     private var speechText : String = ""
+    private var isRecording = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("MB", "Setting content to activity Main")
         setContentView(R.layout.activity_main)
 
+        Log.i("MB", "checking permissions")
         checkPermission()
-        button_record_toggle.setOnCheckedChangeListener { _, isRecording ->
+        Log.i("MB", "setting button")
+        button_record_toggle.setOnClickListener() {_ ->
+            toggleRecording()
             if (isRecording) {
                 Toast.makeText(this, "Recording...", Toast.LENGTH_LONG).show()
                 speechListener.startListening()
@@ -34,6 +40,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        Log.i("MB", "setting results listener")
         result_button.setOnClickListener { _ ->
             Toast.makeText(this, "starting activity", Toast.LENGTH_SHORT).show()
             // TODO: start result activity
@@ -42,6 +49,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun toggleRecording() {
+        isRecording = !isRecording
+        if (isRecording) {
+            button_record_toggle.background = getDrawable(R.drawable.round_button_recording)
+        } else {
+            button_record_toggle.background = getDrawable(R.drawable.round_button)
+        }
+    }
+
+    //update to prompt user to update permissions
     private fun checkPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
